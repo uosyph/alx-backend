96f9aa95aef783013385e9f6f2d4310974890ceb
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-03 19:50:45 +0200

    0x01 - Task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+MRUCache module implementing a Most Recently Used (MRU) based caching system.
+
+This module defines the MRUCache class, which extends the BaseCaching class.
+It provides a caching mechanism where items are added to the cache,
+and when the cache reaches its maximum capacity,
+the most recently used item is removed to make space for the new item.
+"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class implementing a Most Recently Used based caching system.
+    """
+
+    def __init__(self):
+        """
+        Initialize class instance.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.mru = ""
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: The key for the item.
+            item: The item to be stored in the cache.
+
+        Returns:
+            None
+        """
+        if key and item:
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                self.cache_data[key] = item
+                self.mru = key
+            else:
+                if key not in self.cache_data:
+                    discarded_item = self.mru
+                    del self.cache_data[discarded_item]
+                    print(f"DISCARD: {discarded_item}")
+                    self.cache_data[key] = item
+                    self.mru = key
+                else:
+                    self.cache_data.update({key: item})
+                    self.mru = key
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Args:
+            key: The key associated with the item to retrieve.
+
+        Returns:
+            The cached item if found,
+            or None if the key is not present in the cache.
+        """
+        if key in self.cache_data:
+            self.mru = key
+            return self.cache_data[key]
---
+++ 0x01-caching/tests/4-main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 4-main """
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+MRUCache = __import__('4-mru_cache').MRUCache
+
+my_cache = MRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
