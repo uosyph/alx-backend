0248f46e7685ed3216b29597e5a1ddcc8b60d882
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-09 20:13:31 +0200

    0x02 - Tasks (4,5)

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Route module for the API.
+
+Endpoints:
+    - GET /: Renders the 4-index.html template.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.url_map.strict_slashes = False
+
+
+class Config(object):
+    """Configuration for language settings."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("4-app.Config")
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """
+    Handle GET requests on the root endpoint.
+
+    Returns:
+        Rendered template: 4-index.html
+    """
+    return render_template("4-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best-matching supported language based on client preferences.
+
+    Returns:
+        str: The best-matching language code from the available languages.
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""
+Route module for the API.
+
+Endpoints:
+    - GET /: Renders the 5-index.html template.
+"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.url_map.strict_slashes = False
+
+
+class Config(object):
+    """Configuration for language settings."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("5-app.Config")
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """
+    Handle GET requests on the root endpoint.
+
+    Returns:
+        Rendered template: 5-index.html
+    """
+    return render_template("5-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best-matching supported language based on client preferences.
+
+    Returns:
+        str: The best-matching language code from the available languages.
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user() -> Union[dict, None]:
+    """
+    Retrieve user information based on the 'login_as' query parameter.
+
+    Returns:
+        Union[dict, None]: A dictionary containing user information if found,
+        otherwise None.
+    """
+    login_as = request.args.get("login_as")
+    if login_as:
+        return users.get(int(login_as))
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Execute before each request to find and set
+    user information in the global Flask context.
+
+    The user information is stored in 'flask.g.user'.
+    """
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
