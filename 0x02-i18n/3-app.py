dc753ef8fcb56a2a9b2966b9f48b91ab1596243a
Yousef Saeed <yousafesaeed@gmail.com>
2024-01-09 15:51:00 +0200

    0x02 - Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Route module for the API.
+
+Endpoints:
+    - GET /: Renders the 3-index.html template.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.url_map.strict_slashes = False
+
+
+class Config(object):
+    """Configuration for language settings."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("3-app.Config")
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """
+    Handle GET requests on the root endpoint.
+
+    Returns:
+        Rendered template: 3-index.html
+    """
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best-matching supported language based on client preferences.
+
+    Returns:
+        str: The best-matching language code from the available languages.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
